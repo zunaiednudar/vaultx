46b1e24a1af51116f8667fc3001b98c4c36eae0e
Md. Zunaied Nudar <zunaied4821@gmail.com>
2025-09-13 17:07:45 +0600

    1st commit: branch-zunaied

---
+++ Dashboard.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="Dashboard" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="vaultx.Dashboard" %>
+
+<asp:Content ID="DashboardHead" ContentPlaceHolderID="SiteHead" runat="server">
+    <link rel="stylesheet" href="styles/dashboard.css" type="text/css" />
+</asp:Content>
+
+<asp:Content ID="DashboardMainContent" ContentPlaceHolderID="SiteMainContent" runat="server">
+    <!-- Accounts Section -->
+    <section class="accounts-section">
+        <h2>My Accounts</h2>
+        <div class="accounts-grid">
+            <asp:Repeater ID="rptAccounts" runat="server">
+                <ItemTemplate>
+                    <div class="account-card">
+                        <h3><%# Eval("AccountNumber") %></h3>
+                        <p>Balance: $<%# Eval("Balance") %></p>
+                        <p>Type: <%# Eval("AccountType") %></p>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+
+            <!-- Placeholder cards will be added in code-behind if less than 3 accounts -->
+            <!-- Single placeholder (visible only if needed, added in code-behind) -->
+            <asp:PlaceHolder ID="phAddAccount" runat="server" Visible="false">
+                <div class="account-card add-card">
+                    +
+                </div>
+            </asp:PlaceHolder>
+        </div>
+    </section>
+
+    <!-- Transactions Section -->
+    <section class="transactions-section">
+        <h2>Recent Transactions</h2>
+        <div class="transactions-list">
+            <asp:Repeater ID="rptTransactions" runat="server">
+                <ItemTemplate>
+                    <div class="transaction-item">
+                        <span class="txn-aid"><%# Eval("AccountNumber", "{0:MMM dd, yyyy}") %></span>
+                        <span class="txn-type"><%# Eval("TransactionType") %></span>
+                        <span class="txn-id"><%# Eval("TransactionId") %></span>
+                        <span class="txn-amount">$<%# Eval("Amount") %></span>
+                        <span class="txn-reference"><%# Eval("Reference") %></span>
+                        <span class="txn-date"><%# Eval("Date", "{0:MMM dd, yyyy}") %></span>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </section>
+</asp:Content>
---
+++ Dashboard.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace vaultx
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["VaultXDbConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindAccounts();
+                BindTransactions();
+            }
+        }
+
+        private void BindAccounts()
+        {
+            var accounts = new List<dynamic>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                                SELECT AID, AType, ABalance, ACreatedAt
+                                FROM Accounts
+                                WHERE UID = @UID
+                                ORDER BY ACreatedAt ASC
+                                ";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UID", Session["UID"]);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            accounts.Add(new
+                            {
+                                AccountNumber = reader["AID"].ToString(),
+                                Balance = Convert.ToDecimal(reader["ABalance"]),
+                                AccountType = reader["AType"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+
+            rptAccounts.DataSource = accounts;
+            rptAccounts.DataBind();
+
+            // Show + if less than 3 accounts
+            phAddAccount.Visible = (accounts.Count < 3);
+        }
+
+        private void BindTransactions()
+        {
+            var transactions = new List<dynamic>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                                SELECT TOP 10 T.TID, T.AID, T.TrxID, T.TType, T.TAmount, T.TReference, T.TDate
+                                FROM Transactions T INNER JOIN Accounts A ON T.AID = A.AID
+                                WHERE A.UID = @UID
+                                ORDER BY T.TDate DESC
+                                ";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UID", Session["UID"]);
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            transactions.Add(new
+                            {
+                                AccountNumber = reader["AID"].ToString(),
+                                TransactionType = reader["TType"].ToString(),
+                                TransactionId = reader["TrxID"].ToString(),
+                                Amount = Convert.ToDecimal(reader["TAmount"]),
+                                Reference = reader["TReference"].ToString(),
+                                Date = Convert.ToDateTime(reader["TDate"])
+                            });
+                        }
+                    }
+                }
+            }
+
+            rptTransactions.DataSource = transactions;
+            rptTransactions.DataBind();
+        }
+    }
+}
